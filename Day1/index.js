f6958f9b14d13ced69bacc310f5221e50aa9935b
Gxavier <103543979+Gxavier7@users.noreply.github.com>
2023-11-03 15:06:02 -0300

    Revert "changes"
    This reverts commit 573c11995a5a6c771d56ff5b1a8e4c5b70af2ff9.

--- Day1/index.js
+++ Day1/index.js
@@ -1,15 +1,5 @@
-const deg = 6;
-  const hr = document.querySelector('#hr');
-  const min = document.querySelector('#min');
-  const sec = document.querySelector('#sec');
-  
-  setInterval(() => {
-    let day = new Date();
-    let hh = day.getHours() * 30 ;
-    let mm = day.getMinutes() * deg;
-    let ss = day.getSeconds() * deg;
-    
-    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
-    min.style.transform = `rotateZ(${mm}deg)`;
-    sec.style.transform = `rotateZ(${ss}deg)`;
-  })
+const index = document.querySelectorAll(".index")
+
+index.forEach( (index) => {
+  index.style.transform = `rotate(${30*index.dataset.calc})`;
+});
